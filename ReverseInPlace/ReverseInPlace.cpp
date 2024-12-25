41f09590dffc2112db53fc76931f5e51d554ce03
BelalBasal <basdibrahim@gmail.com>
2024-12-25 03:43:30 +0200

    v02

--- ReverseInPlace/ReverseInPlace.cpp
+++ ReverseInPlace/ReverseInPlace.cpp
@@ -6,8 +6,26 @@ void ReverseInPlace(int arr[], int Size)
 {
 	for (int i = 0;i < Size / 2;i++)
 	{
-		swap(arr[i], arr[Size - 1]);
+		swap(arr[i], arr[Size-i - 1]);
 	}
+}
+bool TestCase()
+{
+	bool Flag = true;
+	const int Size = 100;
+	int cpy[Size];
+	int arr[Size];
+	for (int i = 0;i < Size;i++)
+	{
+		cpy[i] = arr[i] = rand() % 100;
+	}
+	ReverseInPlace(arr, Size);
+	reverse(cpy, cpy + Size);
+	for (int i = 0; i < Size;i++)
+	{
+		Flag &= (cpy[i] == arr[i]);
+	}
+	return Flag;
 }
 int main()
 {
@@ -17,5 +35,7 @@ int main()
 	{
 		cout << numbers[i] << endl;
 	}
+
+	cout << TestCase();
 }
 
